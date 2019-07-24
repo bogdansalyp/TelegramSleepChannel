2da901eadc2463a93ab3fd185e0bbdfd09ac9883
bsalyp <bsalyp@cisco.com>
2019-07-24 21:41:19 +0300

    Fixed channel name

--- bot.py
+++ bot.py
@@ -2,6 +2,6 @@ import telegram
 from key import token
 
 bot = telegram.Bot(token=token)
-status = bot.send_message(chat_id="@WaterReminder", text="пора спать", parse_mode=telegram.ParseMode.HTML)
+status = bot.send_message(chat_id="@SleepReminder", text="пора спать", parse_mode=telegram.ParseMode.HTML)
 
 print(status)
